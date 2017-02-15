cbe5d7c37267513a133643b3919c017bdf5495f9
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-02-15 14:35:40 +0100

    Imlements the comparison function into unit tests

--- main.c
+++ main.c
@@ -10,11 +10,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "char_list.h"
+#include "sort_splitMerge.h"
 
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_RESET "\x1b[0m"
+
+void unit_test();
 
 int main(int argc, char const *argv[])
 {
@@ -27,10 +30,88 @@ int main(int argc, char const *argv[])
 	myList = fillList(myList,test,16);
 	printf(ANSI_COLOR_BLUE"First display :\n"ANSI_COLOR_RESET);
 	printList(myList);
+	unit_test();
 	emptyList(&myList);
 	if(myList != NULL)
 	{
 		printf(ANSI_COLOR_RED"ERROR ! myList = %x\n"ANSI_COLOR_RESET,myList);
 	}
 	return 0;
+}
+
+void unit_test()
+{
+	printf(ANSI_COLOR_BLUE"Test of string comparison :\n"ANSI_COLOR_RESET);
+	int res = compare_char("1","2");
+	if( res == 2)
+	{
+		printf(ANSI_COLOR_GREEN"\t1 > 2\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t1 > 2 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("2","1");
+	if( res == 1)
+	{
+		printf(ANSI_COLOR_GREEN"\t2 < 1\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t2 < 1 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("1","1");
+	if( res == 0)
+	{
+		printf(ANSI_COLOR_GREEN"\t1 = 1\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t1 = 1 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("10","1");
+	if( res == 1)
+	{
+		printf(ANSI_COLOR_GREEN"\t10 > 1\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t10 > 1 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("10","2");
+	if( res == 1)
+	{
+		printf(ANSI_COLOR_GREEN"\t10 > 2\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t10 > 2 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("1","10");
+	if( res == 2)
+	{
+		printf(ANSI_COLOR_GREEN"\t1 < 10\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t1 < 10 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("2","10");
+	if( res == 2)
+	{
+		printf(ANSI_COLOR_GREEN"\t2 < 10\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\t2 < 10 => %d\n"ANSI_COLOR_RESET, res);
+	}
+	res = compare_char("a","b");
+	if( res == 0)
+	{
+		printf(ANSI_COLOR_GREEN"\ta ; b\n"ANSI_COLOR_RESET);
+	}
+	else
+	{
+		printf(ANSI_COLOR_RED"\ta ; b => %d\n"ANSI_COLOR_RESET, res);
+	}
 }
